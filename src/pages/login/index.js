1606ad1ed91ad7e503000b1adb750f0d73c5f281
Filip Bolčić <fbolcic@span.eu>
2022-01-16 21:00:13 +0100

    Sign in and sign out button visbility logic

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,14 +7,17 @@ const Login = () => {
 
   return (
     <>
-      <button onClick={() => auth.signIn()} className="login-button">
-        <GoogleIcon title="Google icon" className="login-button-icon" />
-        <span className="login-button-text">Sign in</span>
-      </button>
-      <button onClick={() => auth.signOut()} className="login-button">
-        <GoogleIcon title="Google icon" className="login-button-icon" />
-        <span className="login-button-text">Sign out</span>
-      </button>
+      {auth.user ? (
+        <button onClick={() => auth.signOut()} className="login-button">
+          <GoogleIcon title="Google icon" className="login-button-icon" />
+          <span className="login-button-text">Sign out</span>
+        </button>
+      ) : (
+        <button onClick={() => auth.signIn()} className="login-button">
+          <GoogleIcon title="Google icon" className="login-button-icon" />
+          <span className="login-button-text">Sign in</span>
+        </button>
+      )}
     </>
   );
 };
